d8bbb5967cc374b848a938cf7b9e623f613562f0
Roger Veciana <roger.vecianarovira@bp.com>
2021-01-08 15:32:08 +0100

    Solve some bugs

--- src/App.svelte
+++ src/App.svelte
@@ -11,11 +11,19 @@ import type { Bar } from "./model"; //https://github.com/pyoner/svelte-typescrip
 	let canvas: HTMLCanvasElement;
 	let lng = 0;
 	let lat = 0;
+	let utmX = 0;
+	let utmY = 0;
 	
 	let heading = 0;
 	let radius = 1000;
 	let features:Bar[] = [];
 	let amenity = "bar"
+	const amenityOptions = [
+		{value: "bar", label: "Bar"},
+		{value: "restaurant", label: "Restaurant"},
+		{value: "school", label: "School"},
+		{value: "place_of_worship", label: "Place of Worship"}
+	]
 	
 
 	onMount(() => {
@@ -46,27 +54,26 @@ const setHeading = (ev:DeviceOrientationEvent)=>{
 const displayLocationInfo = (position: GeolocationPosition) =>{
 	lng = position.coords.longitude;
 	lat = position.coords.latitude;
-	const {easting: utmX, northing: utmY} = fromLatLon(lat, lng)
+	({easting: utmX, northing: utmY} = fromLatLon(lat, lng));
+}
+
+$: {
+const address = `https://overpass.kumi.systems/api/interpreter?data=[out:json][timeout:25000];node["amenity"="${amenity}"](around:${radius},${lat},${lng});out body;>;out skel qt;`
+
+fetch(address)
+.then(response => response.json())
+.then(data =>{features = data.elements.map(d=>{
+	const  {easting: dataX, northing: dataY} = fromLatLon(d.lat, d.lon);
+	const dist = Math.sqrt((dataX - utmX)**2 + (dataY - utmY)**2);
+	const dir = (180/Math.PI) * Math.atan2((dataX - utmX)/dist, (dataY - utmY)/dist)
 	
-
-
-	const address = `https://overpass.kumi.systems/api/interpreter?data=[out:json][timeout:25000];node["amenity"="${amenity}"](around:${radius},${lat},${lng});out body;>;out skel qt;`
-	fetch(address)
-	.then(response => response.json())
-	.then(data =>{features = data.elements.map(d=>{
-		const  {easting: dataX, northing: dataY, zoneNum: dataZone} = fromLatLon(d.lat, d.lon);
-		const dist = Math.sqrt((dataX - utmX)**2 + (dataY - utmY)**2);
-		const dir = (180/Math.PI) * Math.atan2((dataX - utmX)/dist, (dataY - utmY)/dist)
-		
-		return{name: d.tags.name, lng: d.lon, lat: d.lat, dist, dir}
-		}).filter(d=> d.name!==undefined).sort((a, b)=> a.dist - b.dist);
-		})
-	.catch(error => {
-		console.error('There has been a problem with the overpass API:', error);
-	});
-	
-
-	}
+	return{name: d.tags.name, lng: d.lon, lat: d.lat, dist, dir}
+	}).filter(d=> d.name!==undefined).sort((a, b)=> a.dist - b.dist);
+	})
+.catch(error => {
+	console.error('There has been a problem with the overpass API:', error);
+});
+}
 
 $: canvasSize = Math.min(containerWidth>containerHeight?containerHeight:containerWidth, 500);
 $: drawCompass(canvas, canvasSize, features, heading);
@@ -74,11 +81,15 @@ $: drawCompass(canvas, canvasSize, features, heading);
 
 
 <main bind:clientWidth={containerWidth} bind:clientHeight={containerHeight}>
-<canvas
-	bind:this={canvas}
-	width={canvasSize}
-	height={canvasSize}
-></canvas>
+<canvas bind:this={canvas}/>
+
+<select bind:value={amenity}>
+	{#each amenityOptions as amenityOption}
+	<option value={amenityOption.value} >
+		{amenityOption.label}
+	</option>
+	{/each}
+</select>
 <div class="copyright">
 	© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors
 </div>
